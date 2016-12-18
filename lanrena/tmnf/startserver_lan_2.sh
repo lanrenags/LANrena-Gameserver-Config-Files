4b74539795d5ac20cf239aca4aaf20b715130b88
Christoph Raible <Chr.Raible@gmail.com>
2016-12-18 18:25:17 +0100

    LANrena Configfiles 2016

---
+++ lanrena/tmnf/startserver_lan_2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+DIR=`pwd`
+$DIR/TrackmaniaServer /game_settings=MatchSettings/war.txt /dedicated_cfg=war_2.txt /noautoquit /lan
---
+++ lanrena/tmnf/startserver_lan_4.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+DIR=`pwd`
+$DIR/TrackmaniaServer /game_settings=MatchSettings/war.txt /dedicated_cfg=war_4.txt /noautoquit /lan
