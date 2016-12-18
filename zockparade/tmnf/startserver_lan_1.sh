c4bebc981a1f2fb97da930782a66c5d261c70191
Christoph Raible <Chr.Raible@gmail.com>
2016-12-18 18:02:26 +0100

    Zockparade-Configfiles-2016

---
+++ zockparade/tmnf/startserver_lan_1.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+DIR=`pwd`
+$DIR/TrackmaniaServer /game_settings=MatchSettings/war.txt /dedicated_cfg=war_1.txt /noautoquit /lan
---
+++ zockparade/tmnf/startserver_lan_3.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+DIR=`pwd`
+$DIR/TrackmaniaServer /game_settings=MatchSettings/war.txt /dedicated_cfg=war_3.txt /noautoquit /lan
